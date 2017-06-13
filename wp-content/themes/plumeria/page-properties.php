b55919cf46e5d527bd402d1d448afd98abfc3173
Alonso Avotz <alonso@avotz.com>
2017-06-12 19:13:17 -0600

    se agrego pagina propiedades

--- wp-content/themes/plumeria/page-properties.php
+++ wp-content/themes/plumeria/page-properties.php
@@ -38,53 +38,61 @@ get_header(); ?>
 				</div>
 			</section>
 		<?php
-		if ( have_posts() ) : ?>
+
 
 			 
 			<div class="inner">
 				<div class="properties-container">
-				<?php
-				/* Start the Loop */
-				 $left_or_right = 'right'; 
-				while ( have_posts() ) : the_post(); ?>
 
-					<div class="properties-item">
-						<div class="properties-item-img-container">
-							 <?php if ( has_post_thumbnail() ) :
+					<?php
+                        $args = array(
+                          'post_type' => 'property',
+                          'order' => 'ASC',
+                          'posts_per_page' => 10,
+                         
+                          
+                        );
+                        $items = new WP_Query( $args );
+                        $left_or_right = 'right'; 
+                        if( $items->have_posts() ) {
+                          while( $items->have_posts() ) {
+                             $items->the_post();
+                           
+                            ?>
 
-	                              $id = get_post_thumbnail_id($post->ID);
-	                              $thumb_url = wp_get_attachment_image_src($id,'property-thumb', true);
-	                              ?>
-	                              
-	                           
-	                            <div class="properties-item-img layer lazy" data-depth="1.00" data-src="<?php echo $thumb_url[0] ?>" data-ibg-bg="<?php echo $thumb_url[0] ?>">	</div>
-	                          <?php endif; ?>
-							
-						</div>
-						<div class="properties-item-info <?php echo $left_or_right; ?>">
-							<h3><?php the_title(); ?></h3>
-							<?php $terms = wp_get_post_terms( $post->ID, 'type');?>
-							<h6><?php echo $terms[0]->name ?></h6>
-							<hr>
-							<?php the_excerpt() ?>
-							<a href="<?php the_permalink(); ?>" class="btn">Read more</a>
-						</div>
-					</div>
-	                   
-	                  
-	                  
-	                <?php
-	                 $left_or_right = ('right'==$left_or_right) ? 'left' : 'right';  ?>
+                               
+                            <div class="properties-item">
+								<div class="properties-item-img-container">
+									 <?php if ( has_post_thumbnail() ) :
 
-				<?php endwhile;
+			                              $id = get_post_thumbnail_id($post->ID);
+			                              $thumb_url = wp_get_attachment_image_src($id,'property-thumb', true);
+			                              ?>
+			                              
+			                           
+			                            <div class="properties-item-img layer lazy" data-depth="1.00" data-src="<?php echo $thumb_url[0] ?>" data-ibg-bg="<?php echo $thumb_url[0] ?>">	</div>
+			                          <?php endif; ?>
+									
+								</div>
+								<div class="properties-item-info <?php echo $left_or_right; ?>">
+									<h3><?php the_title(); ?></h3>
+									<?php $terms = wp_get_post_terms( $post->ID, 'type');?>
+									<h6><?php echo $terms[0]->name ?></h6>
+									<hr>
+									<?php the_excerpt() ?>
+									<a href="<?php the_permalink(); ?>" class="btn">Read more</a>
+								</div>
+							</div>
+                              
+                            <?php
+                              $left_or_right = ('right'==$left_or_right) ? 'left' : 'right';  
+                          }
+                        }
 
-				 the_posts_pagination( array( 'mid_size' => 2 ) ); 
+                        the_posts_pagination( array( 'mid_size' => 2 ) ); 
+                      ?>
 
-			else :
-
-				get_template_part( 'template-parts/content', 'none' );
-
-			endif; ?>
+				
 				</div>
 			</div>
 		</div><!-- #main -->
